b15a5f9bb70db9b5df482d89caec47f8b419b4b4
marlisonmourao <bentesmourao@gmail.com>
2023-12-02 21:37:05 -0400

    chore: add shadcn setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata: Metadata = {
   title: 'Full Store',
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+    <html className={inter.className} lang="pt-br">
+      <body className="antialiased">{children}</body>
     </html>
   )
 }

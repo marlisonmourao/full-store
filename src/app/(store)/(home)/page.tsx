8decd5e49069740b14c2da75bc96496d47e8736c
marlisonmourao <bentesmourao@gmail.com>
2023-12-03 22:41:46 -0400

    feat: add first discount banner to home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,9 +1,18 @@
 'use client'
 
-import { useSession } from 'next-auth/react'
+import Image from 'next/image'
 
 export default function Home() {
-  const { data } = useSession()
-
-  return <div></div>
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        alt="até 55% de descontos esse mês"
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        sizes="100vw"
+      />
+    </div>
+  )
 }

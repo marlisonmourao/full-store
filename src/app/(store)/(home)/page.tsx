08909866c83eac36e1b7a6169f5a4c056bb44f8d
marlisonmourao <bentesmourao@gmail.com>
2023-12-03 21:06:05 -0400

    feat: add seeds script to prisma and prisma urls products tables

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -5,5 +5,5 @@ import { useSession } from 'next-auth/react'
 export default function Home() {
   const { data } = useSession()
 
-  return <h1 className="text-xl text-zinc-600">{data?.user?.name}</h1>
+  return <div></div>
 }
